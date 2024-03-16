886de7d2ca0f6bd2cc36795a9de7ecf0f673017d
Libin Faby <97769902+libinfaby@users.noreply.github.com>
2024-03-16 17:37:26 +0530

    Updated paths in conf.py

--- conf.py
+++ conf.py
@@ -16,8 +16,8 @@ release = '1.0'
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ['./_templates']
+exclude_patterns = ['./_readthedocs', 'Thumbs.db', '.DS_Store']
 
 
 
@@ -25,4 +25,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['./_static']
