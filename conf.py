74393a590a114fc23648bcd9c62a71e6f54e2d7b
Libin Faby <97769902+libinfaby@users.noreply.github.com>
2024-03-16 18:24:57 +0530

    Update conf.py due to merge conflicts

--- conf.py
+++ conf.py
@@ -16,8 +16,8 @@ release = '1.0'
 
 extensions = []
 
-templates_path = ['./_templates']
-exclude_patterns = ['./_readthedocs', 'Thumbs.db', '.DS_Store']
+templates_path = ['_templates']
+exclude_patterns = ['_readthedocs', 'Thumbs.db', '.DS_Store']
 
 
 
